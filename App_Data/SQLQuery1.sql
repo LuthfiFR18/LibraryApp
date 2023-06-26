016e4b4b6ec6ef9635a4d23a15aab0baa4244b4d
LUTHFI FADHULLAH RASYID <luthfi.rasyid@binus.ac.id>
2023-06-26 23:02:45 +0700

    26/06

---
+++ App_Data/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Borrower]
+(
+	[Borrower_Id] INT NOT NULL PRIMARY KEY, 
+    [Borrower_Name] VARCHAR(50) NOT NULL, 
+    [Borrower_Address] VARCHAR(50) NOT NULL, 
+    [Borrower_Phone] INT NOT NULL, 
+    [Borrower_Email] VARCHAR(50) NOT NULL, 
+    [Loan_Count] INT NOT NULL, 
+    [Return_Date] DATETIME NOT NULL
+)
